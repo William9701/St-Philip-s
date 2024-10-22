99299b96c0c628bbe84ab328da3e32e89a759178
Obi William <williamobi818@gmail.com>
2024-10-22 13:23:15 +0100

    front-end setup of the church website

---
+++ js/main.js
@@ -0,0 +1,393 @@
+ AOS.init({
+ 	duration: 800,
+ 	easing: 'slide'
+ });
+
+(function($) {
+
+	"use strict";
+
+	$(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
+
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	// loader
+	var loader = function() {
+		setTimeout(function() {
+			if($('#ftco-loader').length > 0) {
+				$('#ftco-loader').removeClass('show');
+			}
+		}, 1);
+	};
+	loader();
+
+	// Scrollax
+   $.Scrollax();
+
+
+
+   // Burger Menu
+	var burgerMenu = function() {
+
+		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+
+			event.preventDefault();
+
+			if ( $('#ftco-nav').is(':visible') ) {
+				$(this).removeClass('active');
+			} else {
+				$(this).addClass('active');
+			}
+
+
+
+		});
+
+	};
+	burgerMenu();
+
+
+	var onePageClick = function() {
+
+
+		$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
+	    event.preventDefault();
+
+	    var href = $.attr(this, 'href');
+
+	    $('html, body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top - 70
+	    }, 500, function() {
+	    	// window.location.hash = href;
+	    });
+		});
+
+	};
+
+	onePageClick();
+
+
+	var carousel = function() {
+		$('.home-slider').owlCarousel({
+	    loop:false,
+	    autoplay: true,
+	    margin:0,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:false,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+	};
+	carousel();
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		// 	 timer;
+		// clearTimeout(timer);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			// timer;
+		// timer = setTimeout(function(){
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+			$this.find('.dropdown-menu').removeClass('show');
+		// }, 100);
+	});
+
+
+	$('#dropdown04').on('show.bs.dropdown', function () {
+	  console.log('show');
+	});
+
+	// scroll
+	var scrollWindow = function() {
+		$(window).scroll(function(){
+			var $w = $(this),
+					st = $w.scrollTop(),
+					navbar = $('.ftco_navbar'),
+					sd = $('.js-scroll-wrap');
+
+			if (st > 150) {
+				if ( !navbar.hasClass('scrolled') ) {
+					navbar.addClass('scrolled');
+				}
+			}
+			if (st < 150) {
+				if ( navbar.hasClass('scrolled') ) {
+					navbar.removeClass('scrolled sleep');
+				}
+			}
+			if ( st > 350 ) {
+				if ( !navbar.hasClass('awake') ) {
+					navbar.addClass('awake');
+				}
+
+				if(sd.length > 0) {
+					sd.addClass('sleep');
+				}
+			}
+			if ( st < 350 ) {
+				if ( navbar.hasClass('awake') ) {
+					navbar.removeClass('awake');
+					navbar.addClass('sleep');
+				}
+				if(sd.length > 0) {
+					sd.removeClass('sleep');
+				}
+			}
+		});
+	};
+	scrollWindow();
+
+
+
+	var counter = function() {
+
+		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-causes').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+				$('.number').each(function(){
+					var $this = $(this),
+						num = $this.data('number');
+						console.log(num);
+					$this.animateNumber(
+					  {
+					    number: num,
+					    numberStep: comma_separator_number_step
+					  }, 7000
+					);
+				});
+
+			}
+
+		} , { offset: '95%' } );
+
+	}
+	counter();
+
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.ftco-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .ftco-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn ftco-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft ftco-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight ftco-animated');
+							} else {
+								el.addClass('fadeInUp ftco-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 50, 'easeInOutExpo' );
+					});
+
+				}, 100);
+
+			}
+
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
+	// magnific popup
+	$('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+     gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+
+
+
+
+
+})(jQuery);
+
+
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+const churchProgramPage = document.getElementById('churchProgramPage');
+
+let currentPage = 1;
+
+// Sample church program data (replace with your actual data)
+const churchProgramData = [
+	{
+	"image": "./images/sermon-1.png"
+	},
+	{
+	"program": [
+		{ "time": "10:00 AM", "event": "Welcome and Announcements" },
+		{ "time": "10:15 AM", "event": "Worship" },
+		{ "time": "11:00 AM", "event": "Sermon" },
+		{ "time": "11:45 AM", "event": "Offering" }
+	]
+	},
+	{
+	"program": [
+		{ "time": "7:00 PM", "event": "Prayer Meeting" },
+		{ "time": "7:30 PM", "event": "Bible Study" }
+	]
+	}
+];
+const prayerData = [
+	{
+	  'prayer': "God Guide and protect us from harm today"
+	}
+]
+
+// Function to display the program based on currentPage
+function displayProgramPage() {
+	const data = churchProgramData[currentPage - 1];
+	churchProgramPage.innerHTML = ''; // Clear previous content
+
+	if (data.image) {
+	churchProgramPage.innerHTML += `
+		<div class="image-container">
+		<img src="${data.image}" alt="Church Image" class="img-fluid">
+		</div>
+	`;
+	}
+
+	if (data.program) {
+	churchProgramPage.innerHTML += `
+		<ul class="church-program-list">
+		${data.program.map(item => `<li>${item.time} - ${item.event}</li>`).join('')}
+		</ul>
+	`;
+	}
+
+	// Update button states based on current page
+	prevButton.disabled = currentPage === 1;
+	nextButton.disabled = currentPage === churchProgramData.length;
+}
+
+// Event listeners for buttons
+nextButton.addEventListener('click', () => {
+	currentPage++;
+	displayProgramPage();
+});
+
+prevButton.addEventListener('click', () => {
+	currentPage--;
+	displayProgramPage();
+});
+
+// Initial display
+displayProgramPage();
+
+function displayPrayer() {
+	currentPage = 1; // Reset currentPage for the new data
+	displayProgramPage(prayerData); // Use the prayerData
+  }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const options = {
+        strings: ['following Jesus whereever he goes.', "Jesus Christ Said:", "16 For God so loved the world", "that he gave his only begotten Son", "that whosoever believeth in him should not perish, but have everlasting life."],
+        typeSpeed: 150,
+        backSpeed: 50,
+        backDelay: 3000,
+        loop: true
+    };
+
+    const multiTextElement = document.querySelector('.multi-text');
+    let currentTextIndex = 0;
+    let currentText = '';
+    let isDeleting = false;
+
+    function type() {
+        const fullText = options.strings[currentTextIndex];
+        if (isDeleting) {
+            currentText = fullText.substring(0, currentText.length - 1);
+        } else {
+            currentText = fullText.substring(0, currentText.length + 1);
+        }
+        multiTextElement.textContent = currentText;
+        let typeSpeed = options.typeSpeed;
+        if(isDeleting) {
+            typeSpeed /= 2;
+        }
+        if (!isDeleting && currentText === fullText) {
+            typeSpeed = options.backDelay;
+            isDeleting = true;
+        } else if (isDeleting && currentText === '') {
+            isDeleting = false;
+            currentTextIndex = (currentTextIndex + 1) % options.strings.length;
+        }
+        setTimeout(type, typeSpeed);
+    }
+    type();
+});
